d8f3a5510e4820fff3712811dedf9512752b45bc
dyna-marie <105487397+dyna-marie@users.noreply.github.com>
2022-05-17 23:35:59 +0800

    LogIn works na. pero parecheck ulit :>>

--- src/finalGroupActivity/Registration.java
+++ src/finalGroupActivity/Registration.java
@@ -62,7 +62,7 @@ public class Registration extends JFrame {
 
         try{
             FileWriter writer = new FileWriter("UserInformationDontHack.txt",true);
-            writer.write("" + name + ", " + username + ", " + age + ", " + password + ", " + confirmPassword);
+            writer.write(name + "," + username + "," + age + "," + password + "," + confirmPassword);
             writer.write(System.getProperty("line.separator"));
             writer.close();
             JOptionPane.showMessageDialog(this, "Account Successfully Created");
